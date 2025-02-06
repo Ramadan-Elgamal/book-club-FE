8cc631bec9494e2a3fd1d1912663fc844efbca4a
ramadan.reda12345 <ramadan.reda12345@gmail.com>
2025-02-06 19:41:04 +0000

    auth pages v2

--- src/app/pages/auth/signin/SigninPage.tsx
+++ src/app/pages/auth/signin/SigninPage.tsx
@@ -1,14 +1,18 @@
 // import AuthLayout from "../components/AuthLayout";
-import SigninForm from "../components/SigninForm";
+import AuthForm from "../components/AuthForm";
+import { Image } from '@mantine/core';
 import login from "~/assets/imgs/login.png";
+
 // import { FormType } from "../enum";
 export default function LoginPage() {
   return (
     // <AuthLayout image={""} type={FormType.SIGNIN}>
-    <div className="contaienr mx-auto flex justify-between px-20">
-      <img src={login} alt="login-image" width={400} height={525} />
-      <SigninForm />
+    <div className="w-full max-h-screen px-12  flex justify-between">
+    <div className="w-1/2 h-full px-18  flex justify-center">
+      <Image src={login} alt="register-image" fit="cover"/>
     </div>
+    <AuthForm isSigninOrUp="in"/>
+  </div>
     // </AuthLayout>
   );
 }
--- src/app/pages/auth/signup/SignupPage.tsx
+++ src/app/pages/auth/signup/SignupPage.tsx
@@ -1,13 +1,16 @@
-import SignupForm from "../components/SignupForm";
+import AuthForm from "../components/AuthForm";
+import { Image } from '@mantine/core';
 import register from "~/assets/imgs/register.png";
 
 
 export default function SignupPage() {
   return (
     // <AuthLayout image={logo} type={FormType.SIGNUP}>
-      <div className="container mx-auto flex justify-between px-20">
-        <img src={register} alt="register-image" width={400} height={525} />
-        <SignupForm />
+      <div className="w-full max-h-screen px-12  flex justify-between">
+        <div className="w-1/2 h-full px-18  flex justify-center">
+          <Image src={register} alt="register-image" fit="cover"/>
+        </div>
+        <AuthForm isSigninOrUp="up"/>
       </div>
 
     // </AuthLayout>
