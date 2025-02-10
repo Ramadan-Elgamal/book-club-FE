bbc23829cb2ba30aec521c0ad2194aebeb257e2d
abdosamiir <barakatabdo82@gmail.com>
2025-02-10 07:25:22 +0200

    Add navigation to sign-up and sign-in buttons in Toolbar component

--- src/app/layouts/components/Toolbar/Toolbar.tsx
+++ src/app/layouts/components/Toolbar/Toolbar.tsx
@@ -1,4 +1,5 @@
 import { AppShell, Burger, Button, Group, Image } from "@mantine/core";
+import { useNavigate } from "react-router-dom";
 import logo from "~/assets/imgs/logo.png";
 const Toolbar = ({
   opened,
@@ -8,6 +9,7 @@ const Toolbar = ({
   toggle: () => void;
 }) => {
   const tabs = ["Clubs", "Borrow & Lend", "More"];
+  const navigate = useNavigate();
   return (
     <AppShell.Header withBorder={true} px={20}>
       <Group h="100%">
@@ -24,10 +26,16 @@ const Toolbar = ({
             ))}
           </Group>
           <Group gap="md">
-            <Button variant="outline" c="#402905">
+            <Button
+              variant="outline"
+              c="#402905"
+              onClick={() => navigate("/sign-up")}
+            >
               Sign up
             </Button>
-            <Button color="#76552B">Join now</Button>
+            <Button color="#76552B" onClick={() => navigate("/sign-in")}>
+              Join now
+            </Button>
           </Group>
         </Group>
         <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
